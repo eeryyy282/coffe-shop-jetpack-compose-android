de3fc456c397ee0c8beff06403191e5962d0d8a0
Airi <airiagustus82@gmail.com>
2024-06-27 22:35:20 +0700

    Add search file

--- app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/ui/components/Search.kt
+++ app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/ui/components/Search.kt
@@ -3,13 +3,21 @@ package com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.theme.CoffeeShopJetpackComposeTheme
+import com.learnjetpackcompose.coffeshopjetpackcompose.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,10 +30,24 @@ fun Search(
         onSearch = {},
         active = false,
         onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                contentDescription = null,
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        shape = MaterialTheme.shapes.large,
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
         modifier = Modifier
             .padding(16.dp)
             .fillMaxSize()
-            .heightIn(min = 48.dp)
+            .heightIn(min = 48.dp),
     ) {
 
     }
