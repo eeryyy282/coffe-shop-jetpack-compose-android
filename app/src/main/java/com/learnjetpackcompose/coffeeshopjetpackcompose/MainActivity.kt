9c35c954acbd546ad240f00d00d0e175f75a0b57
Airi <airiagustus82@gmail.com>
2024-07-06 18:17:51 +0700

    Add CategoryItem and add preview to MainActivity

--- app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.learnjetpackcompose.coffeeshopjetpackcompose.model.dummyCategory
+import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.CategoryItem
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.Search
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.theme.CoffeeShopJetpackComposeTheme
 import com.learnjetpackcompose.coffeshopjetpackcompose.R
@@ -37,14 +43,6 @@ fun JetpackCoffeeApp() {
     }
 }
 
-@Preview
-@Composable
-fun JetpackCoffeeAppPreview() {
-    CoffeeShopJetpackComposeTheme {
-        JetpackCoffeeApp()
-    }
-}
-
 @Composable
 fun Banner(
     modifier: Modifier = Modifier
@@ -60,4 +58,36 @@ fun Banner(
         )
         Search()
     }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category = category)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetpackCoffeeAppPreview() {
+    CoffeeShopJetpackComposeTheme {
+        JetpackCoffeeApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryPreview() {
+    CoffeeShopJetpackComposeTheme {
+        CategoryRow()
+    }
+
 }
