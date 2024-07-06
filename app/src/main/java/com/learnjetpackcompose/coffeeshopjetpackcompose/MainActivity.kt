be43e795fa6b365705b10989baf8bad87522eee4
Airi <airiagustus82@gmail.com>
2024-07-06 18:35:24 +0700

    Add menu item and add preview to main activity

--- app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/learnjetpackcompose/coffeeshopjetpackcompose/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -19,8 +21,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.learnjetpackcompose.coffeeshopjetpackcompose.model.Menu
+import com.learnjetpackcompose.coffeeshopjetpackcompose.model.dummyBestSellerMenu
 import com.learnjetpackcompose.coffeeshopjetpackcompose.model.dummyCategory
+import com.learnjetpackcompose.coffeeshopjetpackcompose.model.dummyMenu
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.CategoryItem
+import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.MenuItem
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.Search
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.components.SectionText
 import com.learnjetpackcompose.coffeeshopjetpackcompose.ui.theme.CoffeeShopJetpackComposeTheme
@@ -40,10 +46,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetpackCoffeeApp() {
-    Column {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(title = stringResource(id = R.string.section_favorite_menu))
+        MenuRow(listMenu = dummyMenu)
+        SectionText(title = stringResource(id = R.string.section_best_seller_menu))
+        MenuRow(listMenu = dummyBestSellerMenu)
     }
 }
 
@@ -75,6 +87,22 @@ fun CategoryRow() {
         }
 
     }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = Modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu = menu)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
